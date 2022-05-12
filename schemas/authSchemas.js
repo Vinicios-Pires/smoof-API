09a6c71ce9ef3c2ec7c84d52102a07969224f888
Vinicios Pires <contato.viniciospires@gmail.com>
2022-05-12 18:49:13 -0300

    pequena refatoração: authSchema

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -4,7 +4,7 @@ export const authSignUpSchema = joi.object({
    name: joi.string().required(),
    email: joi.string().email().required(),
    password: joi.string().required(),
-   confirmPassword: joi.ref("password"),
+   confirmPassword: joi.valid(joi.ref("password")).required(),
 });
 
 export const authSignInSchema = joi.object({
